71973bbc8e4533c7ce14c6c539bc24d8a094b1ba
MADANI <101865800+apolocine@users.noreply.github.com>
2026-04-19 01:17:41 +0100

    Delete jar_files directory

--- jar_files/01.1.Test bridge_start_via_API.sh
+++
@@ -1,5 +0,0 @@
- curl -s -X PATCH http://localhost:4567/api/setup/upload-jar \
-     -H 'Content-Type: application/json' \
-     -d '{"action":"start","dialect":"hsqldb","host":"localhost","port":90
-   01,"name":"secuaccessdb","user":"SA","password":""}' 2>&1
-##01.1Test bridge start via API
--- jar_files/12.Drop test table curl.sh
+++
@@ -1,5 +0,0 @@
-##   Drop test table 
-curl -s -X POST http://localhost:8765/query \                             
-     -H 'Content-Type: application/json' \                                   
-     -d '{"sql":"DROP TABLE IF EXISTS \"test_ddl\"","params":[]}' 2>&1       
- 
