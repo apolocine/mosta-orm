60ccbd025ac71734c578b6369e823e3011c71b7e
MADANI <101865800+apolocine@users.noreply.github.com>
2026-04-19 01:16:19 +0100

    Delete jar_files directory

--- jar_files/04.Test_install_modules_first.sh
+++
@@ -1,6 +0,0 @@
-# Test install-modules first
-   curl -s -X POST http://localhost:4567/api/setup/install-modules \
-     -H 'Content-Type: application/json' \                              
-     -d '{"modules":["ticketing","access-control"]}' 2>&1
-# Test install-modules API
-
--- jar_files/damarrage_bridj.sh
+++
@@ -1,3 +0,0 @@
-java -cp hsqldb-2.7.2.jar org.hsqldb.util.SqlTool --inlineRc=   
-   "url=jdbc:hsqldb:hsql://localhost:9001/secuaccessdb,user=SA,password="    
-   --sql="SELECT 1 FROM INFORMATION_SCHEMA.SYSTEM_USERS;" 2>&1
