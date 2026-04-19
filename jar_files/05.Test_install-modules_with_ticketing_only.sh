864f87f43434fbd1532331670bbd911e97842328
MADANI <101865800+apolocine@users.noreply.github.com>
2026-04-19 01:15:30 +0100

    Delete jar_files directory

--- jar_files/05.Test_install-modules_with_ticketing_only.sh
+++
@@ -1,5 +0,0 @@
-## Test install-modules with ticketing only
-curl -s -X POST http://localhost:4567/api/setup/install-modules \         
-     -H 'Content-Type: application/json' \                                   
-     -d '{"modules":["ticketing"]}' 2>&1                                     
-   
--- jar_files/08.Check users in HSQLDB.sh
+++
@@ -1,7 +0,0 @@
-## Check users in HSQLDB
-curl -s -X POST http://localhost:8765/query \                             
-     -H 'Content-Type: application/json' \                                   
-     -d '{"sql":"SELECT \"id\", \"email\", \"firstName\", \"lastName\",      
-   \"status\" FROM \"users\"","params":[]}' 2>&1
-   
-
