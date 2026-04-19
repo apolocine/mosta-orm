2dfeffafbe826a8da12fd974bb9234e88dc56927
MADANI <101865800+apolocine@users.noreply.github.com>
2026-04-19 01:16:50 +0100

    Delete jar_files directory

--- jar_files/01.1.Test bridge manually.sh
+++
@@ -1,7 +0,0 @@
-java --source 11 -cp /home/hmd/dev/MostaGare-Install/SecuAccessPro/jar_   
-   files/hsqldb-2.7.2.jar /home/hmd/dev/MostaGare-Install/SecuAccessPro/no   
-   de_modules/@mostajs/orm/bridge/MostaJdbcBridge.java --jdbc-url            
-   "jdbc:hsqldb:hsql://localhost:9001/secuaccessdb" --user SA --password     
-   "" --port 8765 2>&1 &                                                     
-   sleep 3 && curl -s http://localhost:8765/health 2>&1                      
-## Test bridge manually
