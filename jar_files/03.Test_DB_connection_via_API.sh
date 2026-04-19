961867d825e23f1a7dfc4bdfcbeea38697075aa5
MADANI <101865800+apolocine@users.noreply.github.com>
2026-04-19 01:16:19 +0100

    Delete jar_files directory

--- jar_files/03.Test_DB_connection_via_API.sh
+++
@@ -1,7 +0,0 @@
-##  Test DB connection via API
- curl -s -X POST http://localhost:4567/api/setup/test-db \
-     -H 'Content-Type: application/json' \
-     -d '{"dialect":"hsqldb","host":"localhost","port":9001,"name":"secuac
-   cessdb","createIfNotExists":true}' 2>&1
-  
-
